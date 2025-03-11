a516d809cafad34a4689f3e079d0bdaf0c8df81e
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-03-11 01:28:09 -0400

    switch to critereion
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- benches/repeatedly_build_verilog.rs
+++ benches/repeatedly_build_verilog.rs
@@ -1,30 +1,22 @@
 use std::fs;
 
 use camino::Utf8Path;
-use divan::Bencher;
-use marlin::verilator::{VerilatorRuntime, VerilatorRuntimeOptions};
-use marlin::verilog::prelude::*;
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use marlin::{
+    verilator::{VerilatorRuntime, VerilatorRuntimeOptions},
+    verilog::prelude::*,
+};
 
 #[verilog(src = "examples/verilog-project/src/main.sv", name = "main")]
 pub struct Main;
 
-fn main() {
-    divan::main();
-}
-
-#[divan::bench(sample_count = 5, skip_ext_time = true)]
-fn rebuild_and_test_verilog(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("rebuild_and_test_verilog", |b| {
+        b.iter(|| {
             let bench_artifacts_directory =
                 Utf8Path::new(env!("CARGO_TARGET_TMPDIR"))
                     .join("bench_artifacts");
             let _ = fs::remove_dir_all(&bench_artifacts_directory);
-        })
-        .bench_local_values(|_| {
-            let bench_artifacts_directory =
-                Utf8Path::new(env!("CARGO_TARGET_TMPDIR"))
-                    .join("bench_artifacts");
 
             let runtime = VerilatorRuntime::new(
                 &bench_artifacts_directory,
@@ -41,5 +33,11 @@ fn rebuild_and_test_verilog(bencher: Bencher) {
             assert_eq!(main.medium_output, 0);
             main.eval();
             assert_eq!(main.medium_output, u32::MAX);
-        });
+
+            let _main = black_box(&main);
+        })
+    });
 }
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
