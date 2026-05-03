e0d4e0b1636d23f9ea5e07c97717f0d24af919cd
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2026-05-03 18:55:38 -0400

    bump to veryl 0.20
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- examples/veryl_project/tests/simple_test.rs
+++ examples/veryl_project/tests/simple_test.rs
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use example_veryl_project::Wire;
+use example_veryl_project::{U8Counter, Wire};
 use marlin::{verilator::verilator_version, veryl::prelude::*};
 use snafu::Whatever;
 
@@ -40,6 +40,53 @@ fn forwards_correctly() -> Result<(), Whatever> {
     main.eval();
     println!("{}", main.medium_output);
     assert_eq!(main.medium_output, u32::MAX);
+
+    Ok(())
+}
+
+#[test]
+#[snafu::report]
+fn counter_resets() -> Result<(), Whatever> {
+    let runtime = VerylRuntime::new(
+        VerylRuntimeOptions::default()
+            .call_veryl_build(
+                true, /* warning: not thread safe! don't use if you have
+                      * multiple tests */
+            )
+            .with_inner(|verilator_options| {
+                verilator_options.allow_unsupported_verilator(Some(
+                    verilator_version!(5 020),
+                ))
+            }),
+    )?;
+
+    let mut main = runtime.create_model::<U8Counter>()?;
+
+    main.i_reset = 1;
+
+    main.i_clk = 0;
+    main.eval();
+    main.i_clk = 1;
+    main.eval();
+
+    main.i_reset = 0;
+
+    assert_eq!(main.value, 0);
+
+    main.i_clk = 0;
+    main.eval();
+    main.i_clk = 1;
+    main.eval();
+
+    assert_eq!(main.value, 0);
+
+    main.enable = 1;
+    main.i_clk = 0;
+    main.eval();
+    main.i_clk = 1;
+    main.eval();
+
+    assert_eq!(main.value, 1);
 
     Ok(())
 }
